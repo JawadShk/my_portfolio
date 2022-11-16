6356e4e03844c921de8eebeeea1be471bae78f42
JawadShk <shaikhjawad020@gmail.com>
2022-11-16 18:58:39 +0530

    updated

--- Js/script.js
+++ Js/script.js
@@ -4,3 +4,15 @@ var typed = new Typed(".typing",{
     BackSpeed:40,
     loop:true
 })
+
+window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    
+    document.getElementById("navbar").style.background = "#FF5733";
+  } else {
+   
+    document.getElementById("navbar").style.background = "none";
+  }
+}
