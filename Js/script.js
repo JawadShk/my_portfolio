d335d1ad6d239e6e14efcc48c459e44563c987d3
JawadShk <shaikhjawad020@gmail.com>
2022-11-17 12:08:41 +0530

    updated

--- Js/script.js
+++ Js/script.js
@@ -5,14 +5,48 @@ var typed = new Typed(".typing",{
     loop:true
 })
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
  
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+// function scrollFunction() {
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     
-    document.getElementById("navbar").style.background = "#FF5733";
-  } else {
+//     document.getElementById("navbar").classList
+//   } else {
    
-    document.getElementById("navbar").style.background = "none";
-  }
-}
+//     document.getElementById("navbar").style.background = "none";
+//   }
+// }
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+           $('#navbar').addClass('stick');
+        } else {
+           $('#navbar').removeClass('stick');
+        }
+    });
+  });
+
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+    if (mediaQuery.matches) {
+        $('navbar').addClass('stick');
+    } else {
+        $('#navbar').removeClass('stick');
+    }
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+    } else {
+    btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+});
+    
+    
